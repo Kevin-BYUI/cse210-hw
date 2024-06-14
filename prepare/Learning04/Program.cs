c62bac45ed62c910b25479932a3e16f31b713332
kevinoumaogutu <kogutu55@gmail.com>
2024-06-14 10:18:38 +0300

    Finished prep 1

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+        Console.WriteLine("Please come here");
+        Console.WriteLine("what have we done?");
     }
 }
